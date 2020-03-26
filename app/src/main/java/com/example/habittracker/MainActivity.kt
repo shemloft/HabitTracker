48b2898e95cbbe1942c409af71158551ee248839
shemloft <shem.dar99@gmail.com>
2020-03-26 15:28:12 +0500

    Navigation drawer

--- app/src/main/java/com/example/habittracker/MainActivity.kt
+++ app/src/main/java/com/example/habittracker/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.habittracker
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.navigation.NavController
+import androidx.navigation.NavOptions
 import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.habittracker.data.BundleKeys
 import com.example.habittracker.data.Habit
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity :
     AppCompatActivity(),
     HabitsViewFragment.OnAddClickedListener,
     HabitsViewFragment.OnItemClickedListener,
     HabitsViewFragment.HabitRepository,
-    HabitEditorFragment.OnFormFilledListener {
+    HabitEditorFragment.OnFormFilledListener,
+    NavigationView.OnNavigationItemSelectedListener {
 
-    private var navController: NavController? = null
+    private lateinit var navController: NavController
     private var habits = arrayListOf<Habit>()
 
     override fun getHabits(): ArrayList<Habit> = habits
@@ -22,7 +31,11 @@ class MainActivity :
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        setupActionBarWithNavController(navController, drawerLayout)
+        navView.setupWithNavController(navController)
+        navView.setNavigationItemSelectedListener(this)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -32,22 +45,23 @@ class MainActivity :
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        val savedHabits = savedInstanceState.getSerializable(BundleKeys.HABITS_TO_SAVE) as? ArrayList<Habit>
+        val savedHabits =
+            savedInstanceState.getSerializable(BundleKeys.HABITS_TO_SAVE) as? ArrayList<Habit>
         if (savedHabits != null) {
             habits = savedHabits
-            navController?.navigate(R.id.habitsViewFragment)
+            navController.navigate(R.id.habitsViewFragment)
         }
     }
 
     override fun onAddClicked() {
-        navController?.navigate(R.id.habitEditorFragment)
+        navController.navigate(R.id.habitEditorFragment)
     }
 
     override fun onItemClicked(habit: Habit, position: Int) {
         val bundle = Bundle()
         bundle.putInt(BundleKeys.POSITION, position)
         bundle.putSerializable(BundleKeys.HABIT, habit)
-        navController?.navigate(R.id.habitEditorFragment, bundle)
+        navController.navigate(R.id.habitEditorFragment, bundle)
     }
 
     override fun onFormFilled(habit: Habit, position: Int?) {
@@ -60,6 +74,47 @@ class MainActivity :
         val actualPosition = position ?: habits.size - 1
         val bundle = Bundle()
         bundle.putInt(BundleKeys.CHANGED_POSITION, actualPosition)
-        navController?.navigate(R.id.habitsViewFragment, bundle)
+        navController.navigate(R.id.habitsViewFragment, bundle)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_home -> {
+                val options = NavOptions.Builder().setPopUpTo(R.id.nav_graph, true).build()
+                navController.navigate(R.id.habitsViewFragment, null, options)
+            }
+
+            R.id.nav_info -> {
+                if (isValidDestination(R.id.appInfoFragment))
+                    navController.navigate(R.id.appInfoFragment)
+            }
+        }
+        tryToCloseDrawer()
+        return true
+    }
+
+    private fun isValidDestination(destination: Int): Boolean {
+        return destination != navController.currentDestination?.id
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                tryToCloseDrawer()
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+
+    private fun tryToCloseDrawer(): Boolean {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+            return true
+        }
+        return false
     }
 }
