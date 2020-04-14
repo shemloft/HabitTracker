88faf57b9874e276c93beeb983516d82ca7f02ce
shemloft <shem.dar99@gmail.com>
2020-04-14 15:07:35 +0500

    Habit addition on coroutine

--- app/src/main/java/com/example/habittracker/viewmodel/EditorViewModel.kt
+++ app/src/main/java/com/example/habittracker/viewmodel/EditorViewModel.kt
@@ -3,14 +3,24 @@ package com.example.habittracker.viewmodel
 import androidx.lifecycle.ViewModel
 import com.example.habittracker.data.Habit
 import com.example.habittracker.model.Model
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class EditorViewModel: ViewModel() {
+class EditorViewModel : ViewModel(), CoroutineScope {
+    private val job = SupervisorJob()
 
-    fun onFormFilled(habit: Habit, oldHabit: Habit?) {
-        if (oldHabit == null)
-            Model.addHabit(habit)
-        else
-            Model.replaceHabit(oldHabit, habit)
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job + CoroutineExceptionHandler { _, e -> throw e }
+
+    override fun onCleared() {
+        super.onCleared()
+        coroutineContext.cancelChildren()
     }
 
+    fun onFormFilled(habit: Habit, oldHabit: Habit?) = launch {
+        if (oldHabit == null)
+            withContext(Dispatchers.IO) { Model.addHabit(habit) }
+        else
+            withContext(Dispatchers.IO) { Model.replaceHabit(oldHabit, habit) }
+    }
 }
